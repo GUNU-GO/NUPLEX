a7a88a9aea77e96d1edf6020b129a8720c32a7dc
GUNU-GO <metyu4848@gmail.com>
2020-02-07 18:13:17 +0900

    r

---
+++ Python_Projects/Jump_into_Python/game/sound/echo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def echo_test():
+    print ("echo")
+
---
+++ Python_Projects/Jump_into_Python/test_module.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def multiply(a,b):
+    return a*b
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+if __name__ =='__main__':
+    print(add(1, 4))
+    print(sub(4, 2))
+
+# In[ ]:
+
+
+
+
